abe3212aa74782118263173f892ebec94726e191
vanevery <vanevery@walking-productions.com>
2012-08-06 10:15:51 -0400

    Reworked Uploads and Includes

--- post.php
+++ post.php
@@ -1,21 +1,37 @@
 <?php
-ini_set('display_errors', 'On');
-error_reporting(E_ALL|E_STRICT);
 
+include("include.inc");
 
 // Make sure form was submitted
 if (isset($_POST['form_submitted']) && $_POST['form_submitted'] == "true")
 {
 	$uploadfilename = time() . "_" . rand(1000,9999) . "_" . basename($_FILES['file']['name']);
-	// Make sure apache can write to this folder	
-	$uploaddir = dirname(__FILE__) . "/videouploads/";
-	$uploadfile = $uploaddir . $uploadfilename;
-	$uploadrelativefile = 'http://23.23.89.21/var/www/html/videouploads/'. $uploadfilename;
 	
-	if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile))
+	if (isset($_POST['audio_or_video']) && $_POST['audio_or_video'] == "audio") {
+		$uploadfilepath = DEFAULT_AUDIOFILE_PATH . $uploadfilename;
+	} else {
+		$uploadfilepath = DEFAULT_VIDEOFILE_PATH . $uploadfilename;
+	}
+	
+	if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadfilepath))
 	{
 		// Make sure the file isn't executable and you can delete it if you need
-		chmod($uploadfile, 0666);
+		chmod($uploadfilepath, 0666);
+		
+		$participant_id = DEFAULT_PARTICIPANT_ID;
+		if (isset($_POST['participant_id'])) {
+			$participant_id = $_POST['participant_id'];
+		}
+		
+		sqlConnect();
+		
+		if (isset($_POST['audio_or_video']) && $_POST['audio_or_video'] == "audio") {
+			sqlInsert("insert into audio_files (audio_file, participant_id, performance_id) values ('$uploadfilename', $participant_id, CURRENT_PERFORMANCE_ID)");
+		} else {
+			sqlInsert("insert into video_files (video_file, participant_id, performance_id) values ('$uploadfilename', $participant_id, CURRENT_PERFORMANCE_ID)");		
+		}
+		
+		echo("Uploaded Succeeded!");
 	}
 	else
 	{
